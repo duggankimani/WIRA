8db595fa9d5a07408cf917c86eec3443c831699a
Duggan Kimani <mdkimani@gmail.com>
2013-08-30 18:38:43 +0300

    upload servlet

--- src/com/duggan/workflow/server/servlets/upload/UploadServlet.java
+++ src/com/duggan/workflow/server/servlets/upload/UploadServlet.java
@@ -92,12 +92,12 @@ public class UploadServlet extends UploadAction {
 		Enumeration<String> keys= request.getParameterNames();
 
 		Hashtable<String, Long> receivedFiles = getSessionFiles(request, true);
-		System.err.println("------------------- Parameters ------------- ");
-		while(keys.hasMoreElements()){
-			String key = keys.nextElement();
-			System.err.println(key+""+request.getParameter(key));
-		}		
-		System.err.println("------------------- END Parameters ------------- ");
+//		System.err.println("------------------- Parameters ------------- ");
+//		while(keys.hasMoreElements()){
+//			String key = keys.nextElement();
+//			System.err.println(key+""+request.getParameter(key));
+//		}		
+//		System.err.println("------------------- END Parameters ------------- ");
 		
 		int cont = 0;
 		for (FileItem item : sessionFiles) {
@@ -181,7 +181,7 @@ public class UploadServlet extends UploadAction {
 		Long attachmentId = receivedFiles.get(fieldName);
 		
 		if(attachmentId!=null){
-			System.err.println("#################LOG DElete "+attachmentId);
+			//System.err.println("#################LOG DElete "+attachmentId);
 			AttachmentDaoHelper.delete(attachmentId);
 		}
 //		File file = receivedFiles.get(fieldName);
@@ -190,20 +190,6 @@ public class UploadServlet extends UploadAction {
 //		if (file != null) {
 //			file.delete();
 //		}
-	}
-	
-	@Override
-	public void cancelUpload(HttpServletRequest request) {
-		Enumeration<String> keys= request.getParameterNames();
-
-		Hashtable<String, Long> receivedFiles = getSessionFiles(request, true);
-		System.err.println("------------------- CANCEL Parameters ------------- ");
-		while(keys.hasMoreElements()){
-			String key = keys.nextElement();
-			System.err.println(key+""+request.getParameter(key));
-		}		
-		System.err.println("------------------- CANCEL END Parameters ------------- ");
-		super.cancelUpload(request);
 	}
 	
 	public Hashtable<String, Long> getSessionFiles(HttpServletRequest request, boolean createNewIfNone){
