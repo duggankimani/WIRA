c5e3acc5a6c667b4ba525ce562cb486ed1a47560
TomKim2012 <tosh0948@gmail.com>
2013-10-16 18:25:53 +0300

    Minor UI Improvementst

--- test/com/duggan/workflow/test/ListTest.java
+++ test/com/duggan/workflow/test/ListTest.java
@@ -1,16 +1,34 @@
 package com.duggan.workflow.test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import com.duggan.workflow.shared.model.Comment;
-import com.duggan.workflow.shared.model.Document;
-
 public class ListTest {
-
+	
+	
 	public static void main(String[] args) {
-		List<Comment> lst = new ArrayList<Comment>();
+		List<String> lst = new ArrayList<String>();
+		List<String> lst2 = new ArrayList<String>();
+		
+		String[] things ={"Eggs","lasers", "hats", "pie"};
+		String[] things2 ={ "hats", "pie"};
+		
+		//add array items to list1 
+		for(String y:things2){
+			lst2.add(y);
+		}
+		
+		//Add array items to List 2
+		for(String x:things){
+			lst.add(x);
+		}
+		
+		for(int i=0;i<lst.size();i++){
+			System.out.printf("%s ",lst.get(i));
+		}
+		
+		
+		/*List<Comment> lst = new ArrayList<Comment>();
 		
 		//lst.add(new Document());
 		lst.add(new Comment());
@@ -21,6 +39,10 @@ public class ListTest {
 		for(int i=0; i<lst.size(); i++){
 			Comment obj = lst.get(i);
 			System.out.println(obj);
-		}
+		}*/
+	}
+	
+	public void editList(){
+		
 	}
 }
