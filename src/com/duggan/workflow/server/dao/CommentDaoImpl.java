82fe22d3578e741e4a8914fca338e5e62a98f1dd
Duggan Kimani <mdkimani@gmail.com>
2014-02-27 17:33:43 +0300

    Activities without document id

--- src/com/duggan/workflow/server/dao/CommentDaoImpl.java
+++ src/com/duggan/workflow/server/dao/CommentDaoImpl.java
@@ -1,12 +1,22 @@
 package com.duggan.workflow.server.dao;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.jbpm.task.Comment;
+import org.jbpm.task.Task;
 
 import com.duggan.workflow.server.dao.model.CommentModel;
+import com.duggan.workflow.server.db.DB;
+import com.duggan.workflow.server.helper.auth.LoginHelper;
 import com.duggan.workflow.server.helper.session.SessionHelper;
+import com.duggan.workflow.shared.model.UserGroup;
 
 public class CommentDaoImpl {
 
@@ -52,10 +62,67 @@ public class CommentDaoImpl {
 
 	@SuppressWarnings("unchecked")
 	public List<CommentModel> getAllComments(Long documentId) {
+	
+		if(documentId==null){
+			return getAllComments();
+		}
 		
 		return em.createQuery("FROM CommentModel n where n.documentId=:documentId order by created desc")
 				.setParameter("documentId", documentId)
 				.getResultList();
+
 	}
+
+	public List<CommentModel> getAllComments() {
+
+		String userId = SessionHelper.getCurrentUser().getUserId();
+		
+		List<UserGroup> groups = LoginHelper.getHelper().getGroupsForUser(userId);
+		String groupsIds="";
+		for(UserGroup group: groups){
+			groupsIds = groupsIds.concat(group.getName()+",");
+		}
+		
+		if(groupsIds.isEmpty()){
+			return new ArrayList<>();
+		}
+		groupsIds = groupsIds.substring(0, groupsIds.length()-1);
+		
+		StringBuffer hql = new StringBuffer("Select c.id FROM localcomment c "+
+		//"d.id documentId "+
+		"inner join LocalDocument d on (d.id=c.documentId) "+
+		"left join Task t on (t.processInstanceId=d.processInstanceId) "+
+		"left join OrganizationalEntity owner on (owner.id= t.actualOwner_id and owner.DTYPE='User') "+ 
+		"left join PeopleAssignments_PotOwners potowners on (potowners.task_id=t.id)  "+
+		"where "+
+		"t.archived = 0 and "+ 
+		"(d.createdBy=? or " +
+		" owner.id = ? or "+
+		"( potowners.entity_id = ? or potowners.entity_id in (?) )) " +
+		" order by c.created desc"
+		 );
+		
+		Query query = em.createNativeQuery(hql.toString())
+				.setParameter(1, userId)
+				.setParameter(2, userId)
+				.setParameter(3, userId)
+				.setParameter(4, groupsIds);
+		
+		@SuppressWarnings("unchecked")
+		List<BigInteger> commentIds = query.getResultList(); 
+		
+		List<Long> ids = new ArrayList<>();
+		for(BigInteger id: commentIds){
+			ids.add(id.longValue());
+		}
+		
+		@SuppressWarnings("unchecked")
+		List<CommentModel> comments = em.createQuery("FROM CommentModel where id in (:ids)")
+			.setParameter("ids", ids).getResultList();
+		
+		return comments;
+	}
+	
+	
 
 }
