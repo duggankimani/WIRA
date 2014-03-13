f98a9cf44e4a2ffae57cc228c661450c2c28ed16
Duggan Kimani <mdkimani@gmail.com>
2014-03-13 20:04:35 +0300

    Carousel pop management

---
+++ src/com/duggan/workflow/client/ui/events/CloseCarouselEvent.java
@@ -0,0 +1,36 @@
+package com.duggan.workflow.client.ui.events;
+
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.HasHandlers;
+
+public class CloseCarouselEvent extends
+		GwtEvent<CloseCarouselEvent.CloseCarouselHandler> {
+
+	public static Type<CloseCarouselHandler> TYPE = new Type<CloseCarouselHandler>();
+
+	public interface CloseCarouselHandler extends EventHandler {
+		void onCloseCarousel(CloseCarouselEvent event);
+	}
+
+	public CloseCarouselEvent() {
+	}
+
+	@Override
+	protected void dispatch(CloseCarouselHandler handler) {
+		handler.onCloseCarousel(this);
+	}
+
+	@Override
+	public Type<CloseCarouselHandler> getAssociatedType() {
+		return TYPE;
+	}
+
+	public static Type<CloseCarouselHandler> getType() {
+		return TYPE;
+	}
+
+	public static void fire(HasHandlers source) {
+		source.fireEvent(new CloseCarouselEvent());
+	}
+}
