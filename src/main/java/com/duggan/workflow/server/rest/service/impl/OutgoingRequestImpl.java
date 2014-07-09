25062c1693d9408ff62db218380f30eb41ed8d43
TomKim2012 <tosh0948@gmail.com>
2014-07-09 08:11:01 +0300

    Minor changes

--- src/main/java/com/duggan/workflow/server/rest/service/impl/OutgoingRequestImpl.java
+++ src/main/java/com/duggan/workflow/server/rest/service/impl/OutgoingRequestImpl.java
@@ -17,75 +17,80 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 
-public class OutgoingRequestImpl implements OutgoingRequestService{
+public class OutgoingRequestImpl implements OutgoingRequestService {
 
 	static String serviceUri = null;
-	
+
 	static String propfile = "/general.properties";
-	
-	//String erpUrl = "http://ebusiness-duggansit.rhcloud.com/ebusiness/rest";
-	
+
+	// String erpUrl = "http://ebusiness-duggansit.rhcloud.com/ebusiness/rest";
+
 	static Logger logger = Logger.getLogger(OutgoingRequestImpl.class);
-	
+
 	static Properties properties = new Properties();
-	
-	static{
-		try{
-			InputStream is = OutgoingRequestImpl.class.getResourceAsStream(propfile);
+
+	static {
+		try {
+			InputStream is = OutgoingRequestImpl.class
+					.getResourceAsStream(propfile);
 			properties.load(is);
 			String prop = properties.getProperty("ServiceUri");
-			if(prop!=null){
+			if (prop != null) {
 				serviceUri = prop;
-			}else{
-				logger.warn("Property [ServiceUri] not found, using default value ["+serviceUri+"]");
+			} else {
+				logger.warn("Property [ServiceUri] not found, using default value ["
+						+ serviceUri + "]");
 			}
-		}catch(Exception e){
-//			logger.warn("Error Loading [erpUrl], using default value ["+erpUrl+"] :: Cause "+e.getMessage());
-//			e.printStackTrace();
+		} catch (Exception e) {
+			// logger.warn("Error Loading [erpUrl], using default value ["+erpUrl+"] :: Cause "+e.getMessage());
+			// e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Jersey Client
 	 */
 	private Client jclient;
-	
-	public OutgoingRequestImpl(){
-		DefaultClientConfig config = new DefaultClientConfig(JAXBProviderImpl.class);
+
+	public OutgoingRequestImpl() {
+		DefaultClientConfig config = new DefaultClientConfig(
+				JAXBProviderImpl.class);
 		jclient = Client.create(config);
 		jclient.setConnectTimeout(15000);
 
 	}
-	
+
 	@Override
 	public Response executeCall(Request request) {
 		String uri = null;
-		
+
 		Object URI = request.getContext("serviceURI");
-		if(URI!=null && !URI.toString().isEmpty()){
-			uri =URI.toString();
+		if (URI != null && !URI.toString().isEmpty()) {
+			uri = URI.toString();
 		}
-		
-		if(uri==null){			
-			String log = "Trying ServiceUri Property from classpath:"+propfile+".. ";
-			if(serviceUri==null){
-				logger.info(log+" - NO VALUE FOUND");
-			}else{
-				uri=serviceUri;
-				logger.info(log+" - "+uri);
+
+		if (uri == null) {
+			String log = "Trying ServiceUri Property from classpath:"
+					+ propfile + ".. ";
+			if (serviceUri == null) {
+				logger.info(log + " - NO VALUE FOUND");
+			} else {
+				uri = serviceUri;
+				logger.info(log + " - " + uri);
 			}
 		}
 
 		return executeCall(request, uri);
 	}
-	
+
 	@Override
 	public Response executeCall(Request request, String serviceURI) {
-			
-		logger.info("Submitting Request : "+request);
 
-		if(serviceURI==null || serviceURI.isEmpty()){
-			throw new IllegalArgumentException("REST URI cannot be null for rest service");
+		logger.info("Submitting Request : " + request);
+
+		if (serviceURI == null || serviceURI.isEmpty()) {
+			throw new IllegalArgumentException(
+					"REST URI cannot be null for rest service");
 		}
 
 		WebResource resource = jclient.resource(serviceURI);
@@ -96,7 +101,7 @@ public class OutgoingRequestImpl implements OutgoingRequestService{
 			clientResponse = resource.type(MediaType.APPLICATION_JSON)
 					.accept(MediaType.APPLICATION_JSON)
 					.post(ClientResponse.class, request);
-			
+
 			// response.getHeaders()
 		} catch (Exception e) {
 
@@ -107,45 +112,51 @@ public class OutgoingRequestImpl implements OutgoingRequestService{
 		if (!clientResponse.getClientResponseStatus().equals(
 				ClientResponse.Status.OK)) {
 
-			WiraExceptionModel model = clientResponse.getEntity(WiraExceptionModel.class);
-			
+			WiraExceptionModel model = clientResponse
+					.getEntity(WiraExceptionModel.class);
+
 			throw new RuntimeException(model.getCause());
 		}
 
 		Response response = clientResponse.getEntity(Response.class);
-		
+
 		return response;
 
 	}
-	
-	
+
 	public static void main1(String[] args) {
 		CommandContext ctx = new CommandContext();
-		//ctx.setData("docType", DocType.LPO.name());
+		// ctx.setData("docType", DocType.LPO.name());
 		ctx.setData("subject", "LPO/8023/12");
 		ctx.setData("approvalStatus", "20/09/2013");
-		
-		Request request = new Request("WORKFLOWCALLOUTCOMMAND", null, ctx.getData());
 
-//		//
-//		 marshalXml(request);
+		Request request = new Request("WORKFLOWCALLOUTCOMMAND", null,
+				ctx.getData());
+
+		// //
+		// marshalXml(request);
 		//
 		Response response = new OutgoingRequestImpl().executeCall(request);
-		
-		assert request!=null;
+
+		assert request != null;
 	}
 
 	public void executePostCall(String urlEncodedString) {
+		logger.warn("Executing post: "+urlEncodedString);
 		WebResource resource = jclient.resource(urlEncodedString);
-//		String s = resource.type(MediaType.APPLICATION_FORM_URLENCODED)
-//				.accept(MediaType.TEXT_PLAIN).get(String.class);
+		// String s = resource.type(MediaType.APPLICATION_FORM_URLENCODED)
+		// .accept(MediaType.TEXT_PLAIN).get(String.class);
 		resource.type(MediaType.APPLICATION_FORM_URLENCODED)
 				.accept(MediaType.TEXT_PLAIN).post();
-		//System.err.println(s);
+		// System.err.println(s);
+		
 	}
-	
+
 	public static void main(String[] args) {
-		new OutgoingRequestImpl().executePostCall("http://localhost:8080/wira/ipnserv?sender=ZZZZZ&sender_phone=0022322");
+		new OutgoingRequestImpl()
+				.executePostCall("http://localhost:8030/Paybill_IPN/index.php/paybill/updateDetails?mpesaCode=EV32VZ788&idNo=3421441&clientCode=PB/02555");
+		
+		System.err.println("Executed");
 	}
 
 }
--- src/main/java/org/wira/pioneer/integration/PioneerIntegrationWorkitemHandler.java
+++ src/main/java/org/wira/pioneer/integration/PioneerIntegrationWorkitemHandler.java
@@ -1,5 +1,7 @@
 package org.wira.pioneer.integration;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 
 import org.apache.log4j.Logger;
@@ -12,35 +14,43 @@ import com.duggan.workflow.shared.model.Document;
 
 public class PioneerIntegrationWorkitemHandler implements WorkItemHandler {
 
-	private Logger log = Logger.getLogger(PioneerIntegrationWorkitemHandler.class);
-	
+	private Logger log = Logger
+			.getLogger(PioneerIntegrationWorkitemHandler.class);
+
 	@Override
 	public void executeWorkItem(WorkItem workItem, WorkItemManager workItemMgr) {
 		String uri = workItem.getParameter("uri").toString();
-		Document document = (Document)workItem.getParameter("document");
-		String clCode = document.getValues().get("clCode").getValue().toString();
-		String idNo = document.getValues().get("idNumber").getValue().toString();
-		String mpesaCode = document.getValues().get("mpesaCode").getValue().toString();
+		Document document = (Document) workItem.getParameter("document");
+		String clCode = document.getValues().get("clCode").getValue()
+				.toString();
+		String idNo = document.getValues().get("idNumber").getValue()
+				.toString();
+		String mpesaCode = document.getValues().get("mpesaCode").getValue()
+				.toString();
 
 		send(uri, mpesaCode, idNo, clCode);
-		workItemMgr.completeWorkItem(workItem.getId(), new HashMap<String, Object>());
+		workItemMgr.completeWorkItem(workItem.getId(),
+				new HashMap<String, Object>());
 	}
 
-	private void send(String uri, String mpesaCode, String idNo, String clientCode) {
-		if(!uri.endsWith("/")){
-			uri = uri.substring(0, uri.length()-1);
+	private void send(String uri, String mpesaCode, String idNo,
+			String clientCode) {
+		if (uri.endsWith("/")) {
+			uri = uri.substring(0, uri.length() - 1);
 		}
-		String encodedUrl = uri+"?mpesaCode="+mpesaCode+"&idNo="+idNo+"&clientCode="+clientCode;
+		String encodedUrl="";
+	
+		encodedUrl = uri + "?mpesaCode=" + mpesaCode + "&idNo=" + idNo
+					+ "&clientCode=" + clientCode;
 		
-		System.out.println("Encoded::"+encodedUrl); 
-		log.warn("Encoded uri: "+encodedUrl);
+		System.out.println("Encoded::" + encodedUrl);
+		log.warn("Encoded uri: " + encodedUrl);
 		new OutgoingRequestImpl().executePostCall(encodedUrl);
 	}
 
-		
 	@Override
 	public void abortWorkItem(WorkItem workItem, WorkItemManager workItemMgr) {
-		
+
 	}
 
 }
