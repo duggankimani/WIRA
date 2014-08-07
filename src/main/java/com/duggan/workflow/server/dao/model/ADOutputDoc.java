36052cecd3a6602d301a4cb743ec032fc15d6937
Duggan Kimani <mdkimani@gmail.com>
2014-08-07 16:27:56 +0300

    Output documents

--- src/main/java/com/duggan/workflow/server/dao/model/ADOutputDoc.java
+++ src/main/java/com/duggan/workflow/server/dao/model/ADOutputDoc.java
@@ -1,10 +1,12 @@
 package com.duggan.workflow.server.dao.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+@Entity
 public class ADOutputDoc extends PO {
 
 	/**
