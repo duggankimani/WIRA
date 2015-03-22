247c402d8884bcd6cd37ad05b8e92a73df750728
Duggan Kimani <mdkimani@gmail.com>
2015-03-22 12:15:20 +0300

    Added CaseRegistry, Attachment's views in GenericDocumentView

---
+++ src/test/java/com/duggan/workflow/test/process/TestGetProcessInstances.java
@@ -0,0 +1,38 @@
+package com.duggan.workflow.test.process;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.duggan.workflow.server.db.DB;
+import com.duggan.workflow.server.db.DBTrxProvider;
+import com.duggan.workflow.server.helper.auth.LoginHelper;
+import com.duggan.workflow.server.helper.jbpm.ProcessMigrationHelper;
+import com.duggan.workflow.shared.model.ProcessLog;
+
+public class TestGetProcessInstances {
+
+	@Before
+	public void setup(){
+		DBTrxProvider.init();
+		DB.beginTransaction();
+		ProcessMigrationHelper.init();
+	}
+	
+	@Test
+	public void getAllProcessIntances(){
+		List<ProcessLog> logs = DB.getProcessDao().getProcessInstances(null,null);
+		for(ProcessLog log: logs){
+			System.out.println(log);
+		}
+	}
+	
+	@org.junit.After
+	public void destroy() throws IOException{
+		DB.rollback();
+		LoginHelper.get().close();
+		DB.closeSession();
+	}
+}
