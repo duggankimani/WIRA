5fe577d00b885df632e1c59d01f1677093b5c7ba
duggankimani <mdkimani@gmail.com>
2016-02-29 18:47:53 +0300

    Version Manager - added dynamic version file name

--- src/main/java/com/duggan/workflow/server/helper/jbpm/VersionManager.java
+++ src/main/java/com/duggan/workflow/server/helper/jbpm/VersionManager.java
@@ -9,12 +9,15 @@ import com.duggan.workflow.shared.model.Version;
 public class VersionManager {
 	
 	private Version version;
+	private static final String defaultFileName="Version.properties";
 	private static VersionManager manager;
+	private String fileName="Version.properties";
 	
-	private VersionManager(){
+	private VersionManager(String fileName){
+		this.fileName = fileName;
 		try{
 			Properties props = new Properties();
-			props.load(getClass().getResourceAsStream("/Version.properties"));
+			props.load(getClass().getResourceAsStream("/"+fileName));
 			String buildVersion = props.get("version")==null? "x.x.x": props.get("version").toString();
 			String timestamp = props.get("timestamp")==null? "":props.get("timestamp").toString() ;
 			String buildDate = props.get("build.date")==null? "":props.get("build.date").toString() ;
@@ -32,15 +35,24 @@ public class VersionManager {
 		}
 	}
 	
-	private static VersionManager get(){
-		if(manager==null){
-			manager = new VersionManager();
+	private static VersionManager get(String aFileName){
+		if(manager==null || !manager.fileName.equals(aFileName)){
+			manager = new VersionManager(aFileName);
 		}
 		
 		return manager;
 	}
+	
+	private static VersionManager get(){
+		return get(defaultFileName);
+	}
+	
 	public static Version getVersion(){
 		return get().version;
+	}
+	
+	public static Version getVersion(String versionFileName){
+		return get(versionFileName).version;
 	}
 	
 }
